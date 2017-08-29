4e7d254703070b8f13ffc6e1589ad51386449597
smmakowski <makowski.sm@gmail.com>
2017-08-28 18:27:39 -0700

    create product and quotient functions

--- lab2.c
+++ lab2.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int product(int a, int b) {
+  int c;
+  c = a * b;
+
+  printf("Integers (a,b) and product(c) are : %d, %d, %d\n", a, b, c);
+  return c;
+}
+
+float quotient(float a, float b) {
+  float c;
+  c = a / b;
+
+  printf("Floats (a,b) and quotient(c) are : %d, %d, %d\n", a, b, c);
+}
+
 int main () {
   /* variable definition: */
   int a, b, c;
