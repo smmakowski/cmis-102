a16b0a56578981fb7ef54a5548362b46cffbb98e
smmakowski <makowski.sm@gmail.com>
2017-09-05 23:01:49 -0700

    complete perimeter function

--- lab3.c
+++ lab3.c
@@ -1,8 +1,18 @@
-// C code
-// This program will calculate the area of a right triangle. // Developer: Faculty CMIS102
-// Date: Jan 31, XXXX
 #include <stdio.h>
-  int main () {
+#include <math.h>
+
+float perimeter(float base, float height) {
+    // handle edge case (only pass one argument) by making same as height
+    if (height == 0) {
+      height = base;
+    }
+
+    double hypotenuse = sqrt(pow((double)base, 2.0) + pow((double)height, 2.0));
+    // formula according to google
+    return base + height + (float)hypotenuse;
+}
+
+int main () {
   /* variable definition: */
   float base, height, area;
   /* Prompt user for base */
@@ -12,8 +22,10 @@
   printf("Enter the height of the triangle: \n"); // Input the base
   scanf("%f", &height);
   // Calculate the Area
-  area= 0.5 * (base * height);
+  area = 0.5 * (base * height);
   // Print the result
   printf("Area is : %f\n", area);
+  printf("Perimeter is: %f\n", perimeter(base, height));
+
   return 0;
 }
