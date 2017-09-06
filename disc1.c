f7948ee5ed91a08d92b15df0a21784f929a36683
smmakowski <makowski.sm@gmail.com>
2017-09-05 17:19:48 -0700

    complete discussion1

--- disc1.c
+++ disc1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-
+  printf("Hello, World!\n");
+  printf("\t\"This text is between two quotes\"\n");
+  printf("\t- No one ever\n");
+  printf("\a");
+  
   return 0;
 }
