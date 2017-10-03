6d18845b7cbbeca8e9176525e05d50bc23ce418d
smmakowski <makowski.sm@gmail.com>
2017-10-03 16:08:16 -0700

    complete lab7

--- lab7.c
+++ lab7.c
@@ -2,16 +2,20 @@
 
 int Square(int value);
 int Cube(int value);
+double Shrink(int value);
+double To_Celsius(int value);
+
 int main () {
   /* variable definition: */
   int intValue, menuSelect,Results;
+  double doubleResults;
   intValue = 1;
   // While a positive number
   while (intValue > 0) {
     printf ("Enter a positive Integer\n: ");
     scanf("%d", &intValue);
     if (intValue > 0) {
-     printf ("Enter 1 to calculate Square, 2 to Calculate Cube \n: ");
+     printf ("Enter 1 to calculate Square, 2 to Calculate Cube, 3 to Shrink (divide by 2), or 4 to Convert from Farenheit to Celsius:\n");
      scanf("%d", &menuSelect);
     }
     if (menuSelect == 1) {
@@ -22,8 +26,14 @@ int main () {
      // Call the Cube function
      Results = Cube(intValue);
      printf("Cube of %d is %d\n",intValue,Results);
+    } else if (menuSelect == 3) {
+     doubleResults = Shrink(intValue);
+     printf("%d shrank is %f\n", intValue, doubleResults);
+    } else if (menuSelect == 4) {
+     doubleResults = To_Celsius(intValue);
+     printf("%d Degrees Farenheit is %f Degrees Celsius\n", intValue, doubleResults);
     } else {
-     printf("Invalid menu item, only 1 or 2 is accepted\n");
+     printf("Invalid menu item, only 1, 2, 3 or 4 is accepted\n");
     }
   }
   return 0;
@@ -37,4 +47,12 @@ int Square(int value) {
 /* function returning the Cube of a number */
 int Cube(int value) {
    return value * value * value;
+}
+
+double Shrink(int value) {
+  return (double)value / 2.0;
+}
+
+double To_Celsius(int value) {
+  return ((double)value * 1.8) + 32.0;
 }
