038842d4b4f914202cb17d0120ae512414797e7f
smmakowski <makowski.sm@gmail.com>
2017-10-08 17:59:34 -0700

    Add check for correct input per resub instruction

--- hw4.c
+++ hw4.c
@@ -5,9 +5,6 @@
 
 float ball_drop(int height) {
   float result;
-  if (height <= 0) {
-    return 0;
-  }
 
   result = sqrt((2.0 * (double)height) / GRAVITY);
   return result;
@@ -16,12 +13,21 @@ float ball_drop(int height) {
 int main() {
   int input;
   float result;
-
-  for (int i = 0; i < 5; i++) {
-    printf("Please enter a height to drop a ball from:\n");
+  int count = 0;
+  
+  printf("This program will allow you to calculate how many seconds it takes to drop a ball from n-feet for 5 valid inputs.\n");
+  
+  while (count < 5) {
+    printf("Please enter a height (greater than 0) to drop a ball from:\n");
     scanf("%d", &input);
-    result = ball_drop(input);
-    printf ("It will take %f seconds to drop a ball from %i feet\n", result, input);
+
+    if (input <= 0) {
+        printf("Height cannot be 0 or less than 0\n");
+    } else {
+        result = ball_drop(input);
+        printf ("It will take %f seconds to drop a ball from %i feet\n", result, input);
+        count = count + 1;
+    }
   }
 
   return 0;
